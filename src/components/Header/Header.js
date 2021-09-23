47467cd666c3757852c40c42470718bdbddb3486
Sadman95 <mail.sakib.1995@gmail.com>
2021-09-23 23:08:04 +0600

    fontawesome icon libarary attached and used

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from '../../images/logo.png'
-import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../images/logo.png';
+import './Header.css';
 
 const Header = () => {
+    const basketIcon = <FontAwesomeIcon className="icon" icon={faShoppingBag}></FontAwesomeIcon>
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="" />
+            <div className="heading">
+                {basketIcon}
+                <img className="logo" src={logo} alt="" />
+            </div>
             <nav className="nav">
                 <ul className="menu">
                     <li><a href="/shop">Shop</a></li>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props) => {
     // console.log(props)
     const {name, img, price, seller, stock} = props.item;
+    const cartIcon = <FontAwesomeIcon style={{marginRight: "5px"}} icon={faShoppingCart}></FontAwesomeIcon>;
     return (
         <div className="product-details">
             <div>
@@ -14,7 +17,7 @@ const Product = (props) => {
                 <h1 className="product-price">Price: ${price}</h1>
                 <p><small>By: {seller}</small></p>
                 <p><small><b>{stock} available in stock</b></small></p>
-                <button onClick={()=>props.handleAdd(props.item)} className="btn-add">Add to cart</button>
+                <button onClick={()=>props.handleAdd(props.item)} className="btn-add">{cartIcon}Add to cart</button>
             </div>
         </div>
     );
