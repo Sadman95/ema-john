c93d48edd3668eedb39148825f84df54008488e0
Sadman95 <mail.sakib.1995@gmail.com>
2021-09-24 04:26:14 +0600

    set cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
+        console.log('api called')
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-simple-resources/master/fakeData/products.JSON')
         .then(res => res.json())
-        .then(data => setProducts(data))
+        .then(data => { 
+            setProducts(data)
+            console.log('api data receivd')
+        }
+        )
     }, [])
+
+    useEffect(() =>{
+        console.log('local storage called')
+        const savedProducts = getStoredCart();
+        if(products.length){
+            const savedEmpty = [];
+            for (const key in savedProducts) {
+                const matched = products.find(product => product.key === key)
+                console.log(matched)
+                savedEmpty.push(matched);
+            }
+            setCart(savedEmpty);
+        }
+        
+    }, [products])
 
     
     const handleAdd = (product) => {
-        // console.log(product)
+        console.log(product)
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.key)
     }
 
     return (
